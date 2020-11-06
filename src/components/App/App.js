d88bfbb8f2eccb068002734ffd91b705156d9179
Joey Heintz <joeheintz45@gmail.com>
2020-11-06 16:45:07 -0600

    added HashRoutes to App.js and imported necessary dependencies into appropriate files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.getForms();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
+          <h4>
+            <i>Don't forget it!</i>
+          </h4>
         </header>
-        <br/>
+        <br />
       </div>
     );
   }
